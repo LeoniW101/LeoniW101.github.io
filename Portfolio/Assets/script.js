a3aa6e246664d3b9f1ba06a8689fb873ce7accc9
LeoniW101 <127887702+LeoniW101@users.noreply.github.com>
2023-09-21 12:28:33 +0000

    Upload starter files

---
+++ Portfolio/Assets/script.js
@@ -0,0 +1,42 @@
+// PortFolio Link : https://leoniw101.github.io/
+
+// https://pages.github.com/ - hoe portfolio met GitPages maken
+
+// https://fmontes.com/blog/how-to-create-a-portfolio-as-a-frontend-developer-with-no-experience 
+
+// https://www.freecodecamp.org/news/how-to-build-a-developer-portfolio-website/ Stap voor stap bouwen simpele website
+
+//Gitrepo naar bovenstaande site https://github.com/Ksound22/developer-portfolio/tree/master
+
+
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
